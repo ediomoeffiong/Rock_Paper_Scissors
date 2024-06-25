934bb09bd99c31b695909fa2d47d02d6133eaecb
Covenant <ediomoemmaeffiong@gmail.com>
2024-06-25 08:49:04 +0100

    Working version

--- src/Main.java
+++ src/Main.java
@@ -3,74 +3,102 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Main {
-    public static int max = 2;
-    public static int min = 0;
-    public static int score = 0;
-    public static int tries = 0;
-
-    public static Random random = new Random();
-    public static int randomNum = random.nextInt((max - min + 1) - min);
-
+    public static int yourScore = 0;
+    public static int compScore = 0;
+    public static int r;
+    public static String[] list = {"rock", "paper", "scissors"};
     public static void main(String[] args) {
-        System.out.println("Rock, Paper or Scissors");
+        Random random = new Random();
+        r = random.nextInt(3);
 
-        guess();
-    }
-
-    public static void guess() {
-        if (tries > 0) {
-            clear();
-        }
-
+        System.out.println("Warning: Make sure capslock is off");
         System.out.print("Rock, Paper or Scissors: ");
-        Scanner scanGuess = new Scanner(System.in);
-        String guessNum = scanGuess.nextLine();
-        tries++;
+        Scanner scanChoice = new Scanner(System.in);
+        String choice = scanChoice.nextLine();
 
-        if (guessNum.equals(randomNum)) {
-            System.out.println("Correct!");
-            score++;
-            System.out.println("Your score is: " + score);
+        System.out.println("\n");
 
-            System.out.println("\nDo you want to try again? (Y/N)");
-            Scanner scanOpp = new Scanner(System.in);
-            String tryOpp = scanOpp.nextLine();
-            if (tryOpp.equals("Y") || tryOpp.equals("y")) {
-                /*String[] call = {"call"};
-                Main.main(call);*/
-                System.out.println("\n\n");
-                guess();
-            } else if (tryOpp.equals("N") || tryOpp.equals("n")) {
-                System.out.println("Goodbye!");
-            } else {
-                System.out.println("Invalid input! Better luck next time.");
+        for (int i = 0; i < 3; i++) {
+            switch (choice) {
+                case "rock":
+                    if (list[r].equals("rock")) {
+                        playAgain("draw");
+
+                    } else if (list[r].equals("scissors")) {
+                        playAgain("won");
+                    } else {
+                        playAgain("lost");
+                    }
+                    break;
+                case "paper":
+                    if (list[r].equals("rock")) {
+                        playAgain("won");
+
+                    } else if (list[r].equals("scissors")) {
+                        playAgain("lost");
+                    } else {
+                        playAgain("draw");
+                    }
+                    break;
+                case "scissors":
+                    if (list[r].equals("rock")) {
+                        playAgain("lost");
+
+                    } else if (list[r].equals("scissors")) {
+                        playAgain("draw");
+                    } else {
+                        playAgain("won");
+                    }
+                    break;
+                default:
+                    playAgain("draw");
+                    break;
             }
+        }
+    }
 
+    public static void playAgain(String status) {
+
+
+        if (status.equals("won")) {
+            yourScore++;
+            System.out.println("I chose " + list[r] + ", you won");
+        } else if (status.equals("draw")) {
+            System.out.println("I chose " + list[r] + ", it's a draw");
+        } else if (status.equals("lost")) {
+            compScore++;
+            System.out.println("I chose " + list[r] + ", you lost");
+        } else if (status.equals("end")) {
+            System.exit(0);
         } else {
-            System.out.println("Wrong! The answer is: " + randomNum);
-            System.out.println("Your score is: " + score);
+            System.out.println("\n");
+        }
 
-            System.out.println("\n\nDo you want to try again (Y/N): ");
-            Scanner scanOpp = new Scanner(System.in);
-            String tryOpp = scanOpp.nextLine();
-            if (tryOpp.equals("Y") || tryOpp.equals("y")) {
-                String[] call = {"call"};
-                guess();
-            } else if (tryOpp.equals("N") || tryOpp.equals("n")) {
-                System.out.println("Goodbye!");
+        System.out.println("Your score is " + yourScore);
+        System.out.println("My score is " + compScore);
+        System.out.println("\n");
+        System.out.print("Do you want to play again (Y/N): ");
+        Scanner scanPlay = new Scanner(System.in);
+        String play = scanPlay.nextLine();
+
+        if (play.equals("Y") || play.equals("y")) {
+            System.out.println("\n");
+            String[] call = {"call"};
+            Main.main(call);
+        } else if (play.equals("N") || play.equals("n")) {
+            if (yourScore > compScore) {
+                System.out.println("You are the final winner. Good job!");
+            } else if (compScore > yourScore) {
+                System.out.println("Oops! I'm the final winner. Hurray!");
             } else {
-                System.out.println("Invalid input! Better luck next time.");
+                System.out.println("Oops! It's a draw and none of us wins. Sighs!");
+                playAgain("end");
             }
+        } else {
+            System.out.println("Wrong input!");
+            playAgain("blank");
         }
+
     }
 
-    public static void clear() {
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
